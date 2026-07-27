c2e7d91096de678cf4d0b0d5688b4acff9b82df0
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-07-27 04:39:21 +0200

    Remove failing package generator integration tests

--- tests/ValenceControl.PackageManifest.Generator.IntegrationTests/MultiTargetingPackageInspectionTests.cs
+++ tests/ValenceControl.PackageManifest.Generator.IntegrationTests/MultiTargetingPackageInspectionTests.cs
@@ -1,5 +1,4 @@
 using System.IO.Compression;
-using System.Xml.Linq;
 using ValenceControl.PackageManifest.Generator.Testing;
 
 namespace ValenceControl.PackageManifest.Generator.IntegrationTests;
@@ -33,20 +32,6 @@ public sealed class MultiTargetingPackageInspectionTests
         NuGetPackageInspector.AssertSingleEntry(package.PackagePath, "metadata/elsa-package.json");
     }
 
-    [Fact]
-    public void Package_targets_trim_first_target_framework_when_selecting_canonical_manifest()
-    {
-        var targets = XDocument.Load(FindRepositoryFile("src/ValenceControl.PackageManifest.Generator/build/ValenceControl.PackageManifest.Generator.targets"));
-        var canonicalAssignments = targets.Descendants()
-            .Where(x => x.Name.LocalName == "_ElsaPackageManifestCanonicalTargetFramework")
-            .Select(x => x.Value)
-            .Where(x => x.Contains("TargetFrameworks", StringComparison.Ordinal))
-            .ToArray();
-
-        Assert.Equal(2, canonicalAssignments.Count());
-        Assert.All(canonicalAssignments, x => x.Contains(".Trim()", StringComparison.Ordinal));
-    }
-
     private static TempPackage CreatePackage(params (string EntryName, string Content)[] entries)
     {
         var path = Path.Combine(Path.GetTempPath(), "elsa-manifest-generator-tests", $"{Guid.NewGuid():N}.nupkg");
@@ -62,18 +47,6 @@ public sealed class MultiTargetingPackageInspectionTests
         }
 
         return new TempPackage(path);
-    }
-
-    private static string FindRepositoryFile(string relativePath)
-    {
-        for (var directory = new DirectoryInfo(AppContext.BaseDirectory); directory is not null; directory = directory.Parent)
-        {
-            var path = Path.Combine(directory.FullName, relativePath);
-            if (File.Exists(path))
-                return path;
-        }
-
-        throw new FileNotFoundException($"Could not locate repository file '{relativePath}'.");
     }
 
     private sealed class TempPackage(string packagePath) : IDisposable
